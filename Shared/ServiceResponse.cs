6698ced7746e3d73749da69ec083c32ab82cb899
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-07-02 19:25:16 -0400

    Adding ServiceResponse.

---
+++ Shared/ServiceResponse.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Shared
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; } = String.Empty;
+    }
+}
