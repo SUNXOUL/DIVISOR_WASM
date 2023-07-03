bd90e446aa4d7c094e71cc6dac896666e8fab1e3
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-07-03 18:56:23 -0400

    Adding UI excep Modals in functions.

--- Shared/ServiceResponse.cs
+++ Shared/ServiceResponse.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-
 namespace Shared
 {
     public class ServiceResponse<T>
